3423d55251351236de2835c345440b0d0cf32fc6
JamissonBarbosa <jamissondasilvafilho@gmail.com>
2022-04-02 20:08:00 -0300

    criando banco de dados sqlite

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,15 @@
+import "reflect-metadata"
+
 import express from "express"
-import { Response, Request } from "express"
+
+
+import "./database"
 
 const app = express()
 
 //rotas
-app.get('/metGet', (req: Request, res: Response) => {
-    return res.send("Hello world metodo get nova rota")
-})
-
-app.get('/metGet:uuid', (req: Request, res: Response) => {
-    return res.send("metodo get pesquisar por uuid")
-})
-
-app.post('/metPost', (req: Request, res: Response) => {
-    return res.send("Hello Metodo Poste")
-})
 
 
-app.listen(3000, () => 
-    console.log("Server is running")
+
+app.listen(3000, () => console.log("Server is running")
 )
